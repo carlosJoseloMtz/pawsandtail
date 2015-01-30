b1a194599b40a5c07364e3726dab050bcee50627
Tomás Martínez Arenas <matuser00@gmail.com>
2015-01-30 12:58:13 -0600

    Avancez en "nosotros" y en general
    Se creo un archivo css para el componente "card", asi como su respectivo
    controlador "cardcontroller" y se movieron estilos generales de
    ".imagen" a "jumbotron-narrow.css"

---
+++ static/js/cardcontroller.js
@@ -0,0 +1,41 @@
+/*Tomás Martínez Arenas*/
+/*This script is for read the content that can be change in the "Patitas y Colitas" Site*/
+
+/*The content that can change is:*/
+/*== NOSOTROS ==*/
+var JSON_FILE_NOSOTROS="../../static/xml/secciones/nosotros.json";//Mision,Vision,Filosofia
+
+var JSON_FILES_LOADED= new Array();
+/*This function shows the content in a "jsonfile" base in the "object" selected in a "htmlelementprefix" design
+if the json data has already been loaded it's read from the JSON_FILES_LOADED array instead to open the file*/
+function showJSONInCard(jsonfile,object,htmlelementprefix){
+     if(JSON_FILES_LOADED[jsonfile]==undefined){//The jsondata has not been loaded
+        $.getJSON(jsonfile,function(data){
+                JSON_FILES_LOADED[jsonfile]=data;
+                fillCardWithJSONData(data,object,htmlelementprefix);
+            }
+        );
+     }else{//The jsondata has been already loaded
+        fillCardWithJSONData(JSON_FILES_LOADED[jsonfile],object,htmlelementprefix);
+     }
+
+}
+
+/*This function fill the card based in the "htmlelementprefix" with the specific json "data"*/
+function fillCardWithJSONData(data,object,htmlelementprefix){
+    var objectselected=null;
+    for(var i=0;i<data.ROOT.length;i++){
+        if(data.ROOT[i].apartado==object){
+            objectselected=data.ROOT[i];
+            break;
+        }
+    }
+    if(objectselected!=null){//Fill all the containers with the data:
+        //Imagen:
+        $("#"+htmlelementprefix+"_Imagen").attr("src",objectselected.imagen);
+        //Titulo:
+         $("#"+htmlelementprefix+"_Titulo").html(objectselected.titulo);
+        //Texto:
+        $("#"+htmlelementprefix+"_Texto").html(objectselected.texto);
+    }
+}
