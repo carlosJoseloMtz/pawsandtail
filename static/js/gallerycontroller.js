6becce51796d620d4c61ea72e659243490293239
Tomás Martínez Arenas <matuser00@gmail.com>
2015-02-03 23:21:44 -0600

    Avancez en galería
    Se implemento el modal de bootstrap y se esta modificando el
    funcionamientoo del carrusel para que sea dinamico

--- static/js/gallerycontroller.js
+++ static/js/gallerycontroller.js
@@ -1,16 +1,7 @@
 /*This is the controller for the gallery, it has all the functions used in the "galeria" page.*/
 
 //This function opens an image in a dialog using jquery-ui.
-function previewImage(url){
-    var image=$("#image");
-    image.attr("src",url);
-    image.load(function(){
-        $("#dialog").dialog({
-            modal:true,
-            resizable:false,
-            draggable:false,
-            width:'auto',
-            title:'Imagen'
-        });
-    });
+function previewImage(event){
+   $("#myModal").modal('show');
+   $('#carouselimage').attr("current_image",event.currentTarget.getAttribute("id"));
 }
