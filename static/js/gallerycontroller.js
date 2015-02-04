7664e834542a9dde20a7f7b72c2cc0ebeec25d9e
Tomás Martínez Arenas <matuser00@gmail.com>
2015-02-04 12:02:41 -0600

    Terminado diseño de galeria
    Se hizo un test para el boton "cargar mas..."

--- static/js/gallerycontroller.js
+++ static/js/gallerycontroller.js
@@ -1,7 +1,44 @@
 /*This is the controller for the gallery, it has all the functions used in the "galeria" page.*/
 
-//This function opens an image in a dialog using jquery-ui.
+//This function opens an image in a dialog using bootstrap modal.
 function previewImage(event){
    $("#myModal").modal('show');
-   $('#carouselimage').attr("current_image",event.currentTarget.getAttribute("id"));
+   updateImageDialog(event.currentTarget.getAttribute("id"),event.currentTarget.getAttribute("image_large"),event.currentTarget.getAttribute("alt"));
+}
+//This function load the "next/prev" image and title based in the current image preview in the carousel
+$("#modalCarousel").click("slid",function(event){
+    var image_selected=event.target.getAttribute('data-slide');
+    var current_image_index=   $('#carouselimage').attr("current_image").substring(5,6);
+    var initial_current_image_index=   $('#carouselimage').attr("current_image").substring(5,6);
+    if(image_selected=="prev"){
+        if($("#Image"+(--current_image_index)).length>0){
+             updateImageDialog("Image"+current_image_index,$("#Image"+current_image_index).attr("image_large"),$("#Image"+current_image_index).attr("alt"));
+        }else{
+            current_image_index=initial_current_image_index;
+        }
+    }else if(image_selected=="next"){
+        if($("#Image"+(++current_image_index)).length>0){
+             updateImageDialog("Image"+current_image_index,$("#Image"+current_image_index).attr("image_large"),$("#Image"+current_image_index).attr("alt"));
+        }else{
+            current_image_index=initial_current_image_index;
+        }
+    }
+});
+//This function updates the dialog image and properties in te carousel
+function updateImageDialog(current_image,src,title){
+   $('#carouselimage').attr("current_image",current_image);
+   $('#carouselimage').attr("src",src);
+   $('#myModalLabel').html(title);
+}
+//This function loads a group of "n" more images from the collection to the gallery
+//Note: Just for testing now the "test" element is been cloned.
+function loadMoreImages(){
+    var divclone=document.getElementById("test").cloneNode(true);
+    var limit=divclone.getElementsByTagName("img").length;
+    var last_index=parseInt(divclone.getElementsByTagName("img")[limit-1].getAttribute("id").substring(5,6));
+    for(var r=0;r<limit;r++){
+        divclone.getElementsByTagName("img")[r].setAttribute("id","Image"+(last_index+r+1));
+    }
+    document.getElementById("test").setAttribute("id","");
+    document.getElementById("galeria").appendChild(divclone);
 }
